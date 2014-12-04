3e58b502f26eab02b927919166aec1afc90beffb
John Ellis <deckerego@gmail.com>
2014-12-03 22:05:05 -0500

    Do not need an index page

--- webapp/routes.py
+++ webapp/routes.py
@@ -12,7 +12,7 @@ os.chdir(os.path.dirname(__file__))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".")))
 
 from clock import Clock, ProcessStatus
-from bottle import Bottle, HTTPResponse, static_file, get, put, request, response, template
+from bottle import Bottle, HTTPResponse, static_file, get, put, request, response, redirect, template
 
 application = Bottle()
 application.install(Clock())
@@ -31,7 +31,7 @@ def send_css(filename):
 
 @application.get('/')
 def editor():
-	return template('index')
+	redirect("/clock/edit")
 
 @application.route('/codemirror/<filename:path>')
 def send_js(filename):
