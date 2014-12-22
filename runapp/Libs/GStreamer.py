458b1489c05f663aee95a5c6311c2323698d0450
John Ellis <deckerego@gmail.com>
2014-12-21 20:10:24 -0500

    Fixed resume on play

--- runapp/Libs/GStreamer.py
+++ runapp/Libs/GStreamer.py
@@ -12,5 +12,10 @@ class Speaker():
     def play(self, fileName):
         audio_dir = configuration.get('audio_files')
         filePath = "%s/%s" % (audio_dir, fileName)
+        self.pl.set_state(gst.STATE_READY)
         self.pl.set_property('uri','file://'+os.path.abspath(filePath))
         self.pl.set_state(gst.STATE_PLAYING)
+
+    def stop(self):
+        self.pl.set_state(gst.STATE_READY)
+        
