bfe4aa9dc020ff861aa424fe108100cd4ec15edf
John Ellis <deckerego@gmail.com>
2015-01-19 22:47:52 -0500

    Do not re-play if you are already playing

--- runapp/Libs/GStreamer.py
+++ runapp/Libs/GStreamer.py
@@ -10,16 +10,16 @@ class Speaker():
         self.pl = None
 
     def play(self, fileName):
-        self.pl = gst.element_factory_make("playbin", "player")
-        audio_dir = configuration.get('audio_files')
-        filePath = "%s/%s" % (audio_dir, fileName)
-        self.pl.set_state(gst.STATE_READY)
-        self.pl.set_property('uri','file://'+os.path.abspath(filePath))
-        self.pl.set_state(gst.STATE_PLAYING)
+        if not self.isPlaying():
+            self.pl = gst.element_factory_make("playbin", "player")
+            audio_dir = configuration.get('audio_files')
+            filePath = "%s/%s" % (audio_dir, fileName)
+            self.pl.set_state(gst.STATE_READY)
+            self.pl.set_property('uri','file://'+os.path.abspath(filePath))
+            self.pl.set_state(gst.STATE_PLAYING)
 
     def stop(self):
         self.pl.set_state(gst.STATE_READY)
 
     def isPlaying(self):
         return gst.STATE_PLAYING in self.pl.get_state() if self.pl else False
-
