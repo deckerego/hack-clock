b9af98774511390cb5b01c22f478f09dd8d6b140
John Ellis <deckerego@gmail.com>
2015-02-17 00:31:34 -0500

    Rendering tweaks

--- runapp/Libs/PiTFT.py
+++ runapp/Libs/PiTFT.py
@@ -12,13 +12,15 @@ pygame.mouse.set_visible(False)
     
 size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
 screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
-screen.fill((0, 0, 0))        
-pygame.display.update()
+time_font = pygame.font.SysFont("DejaVu Sans", 64)
+date_font = pygame.font.SysFont("DejaVu Sans", 32)
 
-font = pygame.font.SysFont("comicsansms", 96)
+screen.fill((0, 0, 0))        
 now = datetime.datetime.now()
-text = font.render("%d:%d:%d" % (now.hour, now.minute, now.second), True, (128, 128, 128))
-screen.blit(text, (160 - text.get_width() // 2, 120 - text.get_height() // 2))
+time_text = time_font.render(now.strftime("%H:%M:%S"), True, (128, 128, 128))
+date_text = date_font.render(now.strftime("%d %B %Y"), True, (128, 128, 128))
+screen.blit(time_text, (160 - time_text.get_width() // 2, 120 - time_text.get_height() // 2))
+screen.blit(date_text, (0, 0)) 
 
 pygame.display.update()
 
