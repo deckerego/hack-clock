5b9ebd2379880d09a5a3736f3f229d3f19dac31e
John Ellis <deckerego@gmail.com>
2015-02-17 00:59:13 -0500

    Refactored to fit the seven segment methods

--- runapp/Libs/PiTFT.py
+++ runapp/Libs/PiTFT.py
@@ -3,26 +3,44 @@ import pygame
 import time
 import datetime
 
-os.putenv('SDL_FBDEV', '/dev/fb1') 
-os.putenv('SDL_VIDEODRIVER', 'fbcon')
+class Display():
 
-pygame.display.init()
-pygame.font.init()
-pygame.mouse.set_visible(False)
-    
-size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
-screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
-time_font = pygame.font.SysFont("DejaVu Sans", 64)
-date_font = pygame.font.SysFont("DejaVu Sans", 32)
+    def __init__(self):
+        os.putenv('SDL_FBDEV', '/dev/fb1')
+        os.putenv('SDL_VIDEODRIVER', 'fbcon')
 
-screen.fill((0, 0, 0))        
-now = datetime.datetime.now()
-time_text = time_font.render(now.strftime("%H:%M:%S"), True, (128, 128, 128))
-date_text = date_font.render(now.strftime("%d %B %Y"), True, (128, 128, 128))
-screen.blit(time_text, (160 - time_text.get_width() // 2, 120 - time_text.get_height() // 2))
-screen.blit(date_text, (0, 0)) 
+        pygame.display.init()
+        pygame.font.init()
+        pygame.mouse.set_visible(False)
 
-pygame.display.update()
+        size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
+        self.screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
+        self.time_font = pygame.font.SysFont("DejaVu Sans", 64)
+        self.font_color = (128, 128, 128)
 
-time.sleep(5)
+        self.hour = 0
+        self.minute = 0
+        self.second = 0
+        self.colon = ":"
+        self.evening = "AM"
 
+    def setBrightness(self, level):
+        self.font_color = (17 * level, 17 * level, 17 * level)
+
+    def setColon(self, state=True):
+        self.colon = ":" if state else " "
+
+    def setEvening(self, state=True):
+        self.evening = "PM" if state else "AM"
+
+    def setMinutes(self, minutes):
+        self.minute = minutes
+
+    def setHours(self, hours):
+        self.hour = hours
+
+    def _update(self):
+        screen.fill((0, 0, 0))
+        time_text = time_font.render("%d%s%d %s" % (self.hour, self.colon, self.minute, self.evening), True, self.font_color)
+        screen.blit(time_text, (160 - time_text.get_width() // 2, 120 - time_text.get_height() // 2))
+        pygame.display.update()
