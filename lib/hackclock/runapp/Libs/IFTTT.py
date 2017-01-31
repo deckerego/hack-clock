6caeddeede995ddb3772e3d662cc349c3abdaeb4
John Ellis <deckerego@gmail.com>
2017-01-30 23:08:45 -0500

    Working implementation of IFTTT Maker event send

---
+++ lib/hackclock/runapp/Libs/IFTTT.py
@@ -0,0 +1,37 @@
+import httplib
+import logging
+import json
+import datetime
+
+logger = logging.getLogger('ifttt')
+
+console = logging.StreamHandler()
+console.setLevel(logging.WARNING)
+logger.addHandler(console)
+
+class IFTTT:
+  __SERVER = "maker.ifttt.com"
+  __PORT = 443
+  __KEY = None
+  __HEADERS = { "Content-type": "application/json" }
+  __NAME = "hackclock"
+
+  def __init__(self, clockName, key):
+      self.__NAME = clockName
+      self.__KEY = key
+
+  def sendMakerEvent(self, eventName):
+      if not self.__KEY:
+          logger.error("IFTTT Maker Key is not set! Will not send event.")
+          return
+
+      datejson = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+      body = json.dumps({"value1": self.__NAME, "value2": datejson})
+      logger.info("IFTTT Body: %s" % body)
+
+      conn = httplib.HTTPSConnection(self.__SERVER, self.__PORT)
+      conn.request("POST", "/trigger/wake_up/with/key/%s" % self.__KEY, body, self.__HEADERS)
+
+      response = conn.getresponse()
+      logger.info("IFTTT Response: %s %s" % (response.reason, str(response.status)))
+      conn.close()
