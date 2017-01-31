5303337fd3c06e8148a2f6a13a99fed7fab72d9b
John Ellis <deckerego@gmail.com>
2017-01-30 23:54:32 -0500

    Bump version number

--- package.sh
+++ package.sh
@@ -2,7 +2,7 @@
 
 cd ..
 echo "Compressing file..."
-tar Jcf hackclock_2.0.0-1.orig.tar.xz hack-clock/
+tar Jcf hackclock_2.0.1-1.orig.tar.xz hack-clock/
 
 cd hack-clock
 dpkg-buildpackage -rfakeroot -uc -us
