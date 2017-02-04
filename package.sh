708c42dc096eea4bf6c7b5928913fb02ba67582c
John Ellis <deckerego@gmail.com>
2017-02-04 15:37:11 -0500

    Remove old files before packaging

--- package.sh
+++ package.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+rm home/pi/hack-clock/run_clock.py
+rm home/pi/hack-clock/blocks_clock.xml
+rm home/pi/hack-clock/backups/*
+
 cd ..
 echo "Compressing file..."
 tar Jcf hackclock_2.1.0.orig.tar.xz hack-clock/
