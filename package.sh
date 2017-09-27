cb35280f5965cb067e359206dca9d31e37f89024
John Ellis <deckerego@gmail.com>
2017-09-26 21:42:28 -0400

    Okay I really fixed it this time I swear

--- package.sh
+++ package.sh
@@ -1,13 +1,26 @@
 #!/bin/sh
 
-rm home/pi/hack-clock/run_clock.py
-rm home/pi/hack-clock/blocks_clock.xml
-rm home/pi/hack-clock/backups/run_clock.*
-rm home/pi/hack-clock/backups/blocks_clock.*
+VERSION='2.3.1'
+ARCHP='armhf'
 
+# Remove any testing files
+rm -f home/pi/hack-clock/run_clock.py
+rm -f home/pi/hack-clock/blocks_clock.xml
+rm -f home/pi/hack-clock/backups/run_clock.*
+rm -f home/pi/hack-clock/backups/blocks_clock.*
+
+# Package
 cd ..
 echo "Compressing file..."
-tar Jcf hackclock_2.3.1.orig.tar.xz hack-clock/
+tar Jcf "hackclock_$VERSION.orig.tar.xz" hack-clock/
 
 cd hack-clock
 dpkg-buildpackage -rfakeroot -uc -us
+
+# Cleanup
+echo "Cleaning up artifacts..."
+rm "../hackclock_$VERSION-1_$ARCHP.buildinfo"
+rm "../hackclock_$VERSION-1_$ARCHP.changes"
+rm "../hackclock_$VERSION-1.debian.tar.xz"
+rm "../hackclock_$VERSION-1.dsc"
+rm "../hackclock_$VERSION.orig.tar.xz"
